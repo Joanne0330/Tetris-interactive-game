e7c8a6b5051be7d2af840627d937b40952d79a8d
Joanne Chen <joanne_r_chen@yahoo.com>
2020-10-09 18:33:43 +0100

    now my T piece drops one second at a time

--- tetris.js
+++ tetris.js
@@ -3,8 +3,6 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.clientWidth, canvas.height);
 
 const matrix = [
     [0, 0, 0],
@@ -12,11 +10,44 @@ const matrix = [
     [0, 1, 0]
 ]
 
-matrix.forEach((row,y) => {
-    row.forEach((value, x) => {
-        if (value !== 0 ) {
-            context.fillStyle = 'red';
-            context.fillRect(x, y, 1, 1);
-        }
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row,y) => {
+        row.forEach((value, x) => {
+            if (value !== 0 ) {
+                context.fillStyle = 'red';
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        });
     });
-});
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+  
+    dropCounter += deltaTime;
+    if(dropCounter > dropInterval) {
+        player.pos.y++;
+        dropCounter = 0;
+    }
+
+    draw();
+    requestAnimationFrame(update);
+}
+
+const player = {
+    pos: {x: 5, y: 5},
+    matrix: matrix
+}
+
+update();
