65757a041ecb7c5488b76ab95958b2e350c1baf9
Joanne Chen <joanne_r_chen@yahoo.com>
2020-10-10 12:37:51 +0100

    if pieces form a line then it disappears

--- tetris.js
+++ tetris.js
@@ -4,11 +4,23 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 
-const matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0]
-]
+// const matrix = [
+//     [0, 0, 0],
+//     [1, 1, 1],
+//     [0, 1, 0]
+// ]
+function arenaSweep() {
+    outer: for(let y = arena.length - 1; y > 0; --y) {
+        for(let x = 0; x < arena[y].length; ++x) {
+            if(arena[y][x] ===0) {
+                continue outer;
+            }
+        }
+        const row = arena.splice(y, 1)[0].fill(0)
+        arena.unshift(row);
+        ++y;
+    }
+}
 
 function collide(arena, player) {
     const[m, o] = [player.matrix, player.pos];
@@ -113,6 +125,7 @@ function playerDrop(){
         player.pos.y--;
         merge(arena, player);
         playerReset();
+        arenaSweep();
     }
     dropCounter = 0;
 }
